bbbb3d3cecab1762a94bfda56657429bc057f14b
李發財 <liyanjie8712@outlook.com>
2023-04-08 11:16:39 +0800

    +

---
+++ src/Liyanjie.ValueObjects/DateTimeRange.cs
@@ -0,0 +1,14 @@
+﻿namespace Liyanjie.ValueObjects;
+
+public class DateTimeRange : ValueObject
+{
+    public DateTime Begin { get; set; }
+
+    public DateTime End { get; set; }
+
+    protected override IEnumerable<object?> GetAtomicValues()
+    {
+        yield return Begin;
+        yield return End;
+    }
+}
