fe6a36e423fccc12d60978fd30faf8d7b389db8a
李發財 <liyanjie2048@outlook.com>
2023-12-02 19:56:22 +0800

    +

---
+++ src/Liyanjie.ValueObjects/SensitiveData.cs
@@ -0,0 +1,25 @@
+﻿namespace Liyanjie.ValueObjects;
+
+/// <summary>
+/// 敏感数据
+/// </summary>
+public class SensitiveData : ValueObject
+{
+    public string? Origin { get; set; }
+    public string? Modification { get; set; }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public bool? Status { get; set; }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <returns></returns>
+    protected override IEnumerable<object?> GetAtomicValues()
+    {
+        yield return Origin;
+        yield return Modification;
+    }
+}
