c7ee3b9c7118dd1ec1f3ee4bebd2fc44fc3a4cae
李發財 <liyanjie2048@outlook.com>
2024-03-03 16:41:41 +0800

    +

---
+++ src/Liyanjie.Protobuf.Surrogates/Nullable`1.cs
@@ -0,0 +1,11 @@
+﻿namespace Liyanjie.Protobuf.Surrogates;
+
+[ProtoContract]
+public class Nullable<TValue>
+{
+    [ProtoMember(1)]
+    public TValue? Value { get; set; }
+
+    public static implicit operator Nullable<TValue>(TValue? value) => new() { Value = value };
+    public static implicit operator TValue?(Nullable<TValue> value) => value.Value;
+}
